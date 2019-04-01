4fcbb91e8d650bf440e5450929cfb83c0598e532
Dan Myles <daniel.myles@monash.edu>
2019-04-01 18:06:06 +1100

    Started chopping up the scripts to make things a little shorter

--- task/code/development/stimuli/call_grid.m
+++ task/code/development/stimuli/call_grid.m
@@ -1,24 +1,14 @@
-%% Script to draw a simple grid on screen
+%% Script to draw a grid on screen
 % Based loosely on http://peterscarfe.com/ptbtutorials.html
-
-% Call setup scripts
-% setup_screen
 
 % Width of the grid lines.
 penWidthPixels = 5;
 
-% Draw the rect to the screen
 % Set Colour to black (0)
-% Iterate through all positions
+% Iterate through all grid positions and draw a FrameRect
 for i = 1:9
 Screen('FrameRect', window, 0, reelInfo.grid_position{i}, penWidthPixels);
 end
 
 % Flip to the screen
 Screen('Flip', window);
-
-% Wait for a key press
-KbStrokeWait;
-
-% Clear the screen
-% sca;
---
+++ task/code/development/stimuli/setup_screen.m
@@ -0,0 +1,45 @@
+%% Script to run basic Screen Set up
+% Based on http://peterscarfe.com/ptbtutorials.html
+
+% Clear the workspace and the screen
+sca;
+close all;
+clearvars;
+
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
+Screen('Preference', 'SkipSyncTests', 1);  
+
+% Get the screen numbers. This gives us a number for each of the screens
+% attached to our computer.
+% For help see: Screen Screens?
+screens = Screen('Screens');
+
+% We select the minimum of these numbers if we plan to draw to our laptop or main screen. 
+% If we choose maximum this will set up a situation where when
+% have two screens attached to our monitor we will draw to the external
+% screen.
+% screenNumber = max(screens);
+screenNumber = min(screens);
+
+% Define white (white will be 1 and black 0). This is because
+% luminace values are (in general) defined between 0 and 1.
+% For help see: help BlackIndex
+black = BlackIndex(screenNumber);
+white = WhiteIndex(screenNumber);
+
+% For help see: Screen OpenWindow?
+% Open the main window with multi-sampling for anti-aliasing
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, white, [], [], [], [], 6, []);
+
+% Get the size of the on screen window in pixels
+% For help see: Screen WindowSize?
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+% Get the centre coordinate of the window in pixels
+% For help see: help RectCenter
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Screen X positions of our three rectangles
+splitXpos = [screenXpixels * 0.25 screenXpixels * 0.5 screenXpixels * 0.75];
+splitYpos = [screenYpixels * 0.75 screenYpixels * 0.5 screenYpixels * 0.25];
