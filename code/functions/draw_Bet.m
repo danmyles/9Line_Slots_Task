c307b3887cc4bc0033d762d7300269855b3cea8b
Dan <dan.myles@monash.edu>
2022-05-04 14:10:12 +1000

    draw_Bet
    Size textbox to textsize

--- code/functions/draw_Bet.m
+++ code/functions/draw_Bet.m
@@ -59,26 +59,29 @@ xLR = [xL, xR];
 % Colour block
 Screen('FillRect', screenInfo.window, reelInfo.colours(2, :), colourBlock{side(1)}, screenInfo.gridPenWidthPixel);
 
-Screen('TextSize', screenInfo.window, reelInfo.TextSize + 30);
+
+textSize = reelInfo.TextSize + 30;
+Screen('TextSize', screenInfo.window, textSize);
 Screen('TextFont', screenInfo.window, reelInfo.Font);
 
 % Subset draw positions using the random draw in side
 
 DrawFormattedText(screenInfo.window, 'BLUE GAME', 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(1)), rectL(2) + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), textSize], xLR(side(1)), rectL(2) + inset));
 
-Screen('TextSize', screenInfo.window,  reelInfo.TextSize + 10);
+textSize = reelInfo.TextSize + 10;
+Screen('TextSize', screenInfo.window, textSize);
 
 DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1) * 9), ' CREDITS PER SPIN '], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(1)), rectL(2) * 2 + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), textSize], xLR(side(1)), rectL(2) * 2 + inset));
 
 Screen('TextSize', screenInfo.window, reelInfo.TextSize);
 
 DrawFormattedText(screenInfo.window, ['CHOSEN ', num2str(reelInfo.betAChoices), ' TIMES '], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(1)), rectL(2) * 4));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xLR(side(1)), rectL(2) * 4));
 
 DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1)), ' CREDITS x 9 LINES'], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(1)), screenInfo.yCenter));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xLR(side(1)), screenInfo.yCenter));
 
 % ------------------------------------------------------------------------
 % GREEN GAME - B
@@ -87,23 +90,25 @@ DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1)), ' CREDITS x 
 % Colour block
 Screen('FillRect', screenInfo.window, reelInfo.colours(4, :), colourBlock{side(2)}, screenInfo.gridPenWidthPixel);
 
-Screen('TextSize', screenInfo.window, reelInfo.TextSize + 30);
+textSize = reelInfo.TextSize + 30;
+Screen('TextSize', screenInfo.window, textSize);
 
 DrawFormattedText(screenInfo.window, 'GREEN GAME', 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(2)), rectL(2) + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), textSize], xLR(side(2)), rectL(2) + inset));
 
-Screen('TextSize', screenInfo.window,  reelInfo.TextSize + 10);
+textSize = reelInfo.TextSize + 10;
+Screen('TextSize', screenInfo.window, textSize);
 
 DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1) * 9), ' CREDITS PER SPIN '], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(2)), rectL(2) * 2 + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), textSize], xLR(side(2)), rectL(2) * 2 + inset));
 
 Screen('TextSize', screenInfo.window, reelInfo.TextSize);
 
 DrawFormattedText(screenInfo.window, ['CHOSEN ', num2str(reelInfo.betBChoices), ' TIMES '], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(2)), rectL(2) * 4));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xLR(side(2)), rectL(2) * 4));
 
 DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1)), ' CREDITS x 9 LINES'], 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xLR(side(2)), screenInfo.yCenter));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xLR(side(2)), screenInfo.yCenter));
 
 % ------------------------------------------------------------------------
 % DRAW RECTs
@@ -113,13 +118,13 @@ DrawFormattedText(screenInfo.window, [num2str(reelInfo.lineBet(1)), ' CREDITS x 
 Screen('FrameRect', screenInfo.window, screenInfo.black, rectL, screenInfo.gridPenWidthPixel);
 
 DrawFormattedText(screenInfo.window, 'PRESS LEFT', 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xL, rectL(2) * 4 + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xL, rectL(2) * 4 + inset));
 
 % Right
 Screen('FrameRect', screenInfo.window, screenInfo.black, rectR, screenInfo.gridPenWidthPixel);
 
 DrawFormattedText(screenInfo.window, 'PRESS RIGHT', 'center', 'center', ...
-    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), 24], xR, rectL(2) * 4 + inset));
+    [],[],[],[],[],[],CenterRectOnPoint([0, 0, rectL(4) - rectL(2), reelInfo.TextSize], xR, rectL(2) * 4 + inset));
 
 % ------------------------------------------------------------------------
 % DISPLAY CREDITS
